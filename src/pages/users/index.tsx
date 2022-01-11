f8fec949560f9b4b8728208e24bfb61d4cfc2bf2
LuizSouz404 <luiz.souz404@gmail.com>
2022-01-11 17:27:06 -0300

    :construction: Add miragejs and create a fake db
    Using React-query, miragejs, axios and faker to make fake data to testings

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,17 +1,39 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, HStack, Icon, Spinner, Stack, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine, RiPencilLine, RiRestartLine } from "react-icons/ri";
+import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
+  const { data, isLoading, isFetching, refetch, error } = useQuery("users", async () => {
+    const { data } = await api.get("users");
+
+      const user = data.users.map(user => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pr-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+          })
+        }
+      })
+
+      return user;
+  }, {
+    staleTime: 1000 * 60
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
-  })
-
+  });
   return (
     <Box>
       <Header />
@@ -21,57 +43,81 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
 
-            <Link href="/users/create" passHref>
-              <Button as="a" size="sm" fontSize="sm" colorScheme="blue" leftIcon={<Icon as={RiAddLine} fontSize="20"/>}>
-                Criar novo
+            <HStack>
+              <Button onClick={() => refetch()} as="a" size="sm" fontSize="sm" colorScheme="blue" leftIcon={<Icon as={RiRestartLine} fontSize="20"/>}>
+                Atualizar
               </Button>
-            </Link>
+              <Link href="/users/create" passHref>
+                <Button as="a" size="sm" fontSize="sm" colorScheme="blue" leftIcon={<Icon as={RiAddLine} fontSize="20"/>}>
+                  Criar novo
+                </Button>
+              </Link>
+            </HStack>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="blue" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ): error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ): (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="blue" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="blue"/>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Luiz Souza</Text>
-                    <Text fontSize="sm" color="gray.300">luiz@dev.io</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>10 de Janeiro, 2022</Td>}
-                <Td>
-                  {isWideVersion ? (
-                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}>
-                      Editar
-                    </Button>
+                <Tbody>
+                  {data.map(user => {
+                    return (
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="blue"/>
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                      <Td>
+                        {isWideVersion ? (
+                          <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}>
+                            Editar
+                          </Button>
 
-                  ) : (
-                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple">
-                      <Icon as={RiPencilLine} fontSize="16" />
-                    </Button>
-                  )
-                }
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+                        ) : (
+                          <Button as="a" size="sm" fontSize="sm" colorScheme="purple">
+                            <Icon as={RiPencilLine} fontSize="16" />
+                          </Button>
+                        )
+                      }
+                      </Td>
+                    </Tr>
+                    )
+                  })}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
